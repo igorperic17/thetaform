17e85cb4d3062779447654e5611f8ae8dd82213c
Igor Peric <igor@biomech.us>
2024-07-30 18:14:41 +0200

    sorted examples

---
+++ 4_notebook.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    theta = {
+      source  = "hashicorp.com/edu/theta"
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "theta" {
+  email    = var.email
+  password = var.password
+}
+
+# Fetch the list of organizations
+data "theta_organizations" "org_list" {}
+
+# Fetch the list of projects for the first organization
+data "theta_projects" "projects" {
+  organization_id = data.theta_organizations.org_list.organizations[0].id
+}
+
+# Create a deployment template
+resource "theta_deployment_template" "my_first_tf_managed_template" {
+  name              = "notebooktemplate"
+  project_id        = data.theta_projects.projects.projects[0].id
+  description       = "Basic Jupyter notebook template"
+  container_images  = ["jupyter/base-notebook:latest"]
+  container_port    = 8888
+  container_args    = []
+  env_vars = { }
+  tags         = []
+  icon_url     = ""
+}
+
+# Create a deployment using the deployment template created above
+resource "theta_deployment" "notebook_deployment" {
+  name                = "notebookdeployment"
+  project_id          = data.theta_projects.projects.projects[0].id
+  deployment_image_id = theta_deployment_template.my_first_tf_managed_template.id
+  container_image    = theta_deployment_template.my_first_tf_managed_template.container_images[0]
+  min_replicas       = 1
+  max_replicas       = 1
+  vm_id              = "vm_c1"  # Replace with an actual VM ID if needed
+  annotations        = {
+    tags     = "[\"CodeGen\"]"
+    nickname = ""
+  }
+  auth_username      = "my_user"
+  auth_password      = "my_hackathon_winning_password"
+}
+
+output "deployment_url" {
+  value = theta_deployment.notebook_deployment.deployment_url
+}
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
+# build Thetaform provider from local source code and install it
 go install .
+
+
 # TF_LOG=INFO terraform plan --var-file=local.tfvars
 # TF_LOG=DEBUG terraform destroy --var-file=local.tfvars
 TF_LOG=1 terraform apply --var-file=local.tfvars
